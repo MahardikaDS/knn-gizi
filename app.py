e2e85bb33966c9d29bcc617e8a942b83febb9ac3
MahardikaDS <56812848+MahardikaDS@users.noreply.github.com>
2024-06-24 20:07:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
